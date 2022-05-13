24c84d0cc607dcd66bd4fa043178bf816d1b86ac
IGOR FILIMONOV <100062285+flmnv96@users.noreply.github.com>
2022-05-13 14:16:23 +0500

    доработаны файлы

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 TOKEN = "5365371963:AAFNhX3q4DF2ILgZwYXNVcW1nnCPOfq-y8U"
 keys = {
-    'биткойн': 'BTC',
-    'эфириум': 'ETH',
     'доллар': 'USD',
     'евро': 'EUR',
     'рубль': 'RUB',
--- extensions.py
+++ extensions.py
@@ -11,22 +11,22 @@ class CryptoConverter:
     @staticmethod
     def convert(quote: str, base: str, amount: str):
         if quote == base:
-            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты\n{amount} {base} равен {amount} {base}.')
 
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {quote}.')
+            raise ConvertionException(f'Не удалось обработать валюту "{quote}".')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {base}.')
+            raise ConvertionException(f'Не удалось обработать валюту "{base}".')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise ConvertionException(f'Не удалось обработьта количество {amount}.')
+            raise ConvertionException(f'Не удалось обработьта количество "{amount}".')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
